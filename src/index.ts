fc439f74f025dc6b332fd1e1cc66f05fdef58927
Romain Racamier-Lafon <romain.racamier@gmail.com>
2018-05-09 18:15:30 +0200

    reading photos

--- src/index.ts
+++ src/index.ts
@@ -1,48 +1,67 @@
 import chalk from 'chalk'
 import * as figures from 'figures'
+import { readdirSync, statSync } from 'fs'
+import * as globby from 'globby'
 import inquirer from 'inquirer'
 import * as minimist from 'minimist'
-import { readdirSync, statSync } from 'fs'
-import { join } from 'path'
+import { basename, join } from 'path'
 
 const argv = minimist(process.argv.slice(2))
-let path = process.cwd()
-let dirs = []
+let basepath = process.cwd()
+const dirs = []
 
-function log(...thing: any[]): void {
+function log(...thing): void {
   console.log(...thing) // tslint:disable-line:no-console
 }
 
-function error(...thing: any[]): void {
+function error(...thing): void {
   console.error(chalk.redBright(figures.cross, ...thing)) // tslint:disable-line:no-console
 }
 
 function getDirectories(path) {
-  return readdirSync(path).filter(function(file) {
+  return readdirSync(path).filter((file) => {
     return statSync(path + '/' + file).isDirectory()
   })
 }
 
 function getPath() {
   if (argv.path) {
-    path = argv.path
+    basepath = argv.path
   }
-  log('will use path :', path)
+  log('will use basepath :', basepath)
 }
 
 function getDirs() {
-  getDirectories(path).map((dir) => {
+  getDirectories(basepath).map((dir) => {
     // dir will be succesivly 2013, 2014,...
-    const subDir = join(path, dir)
+    const subDir = join(basepath, dir)
     getDirectories(subDir).forEach((sub) => dirs.push(join(subDir, sub)))
   })
-  log('found dirs', dirs)
+  // log('found dirs', dirs)
+}
+
+async function checkNext() {
+  // extract first
+  const dir = dirs.shift() // full path
+  const dirName = basename(dir) // directory/folder name
+  log('reading dir "' + dirName + '"')
+  const dateMatches = dirName.match(/(\d{4})\-(\d{2})/)
+  if (!dateMatches || !dateMatches.length || dateMatches.length !== 3) {
+    return error('failed at detecting year & month')
+  }
+  const year = dateMatches[1]
+  const month = dateMatches[2]
+  log('detected year "' + year + '" and month "' + month + '"')
+  const photos = await globby(join(dir, '**/*.(jpg|jpeg)'))
+  log('found', photos.length, 'photos')
+  log(photos)
 }
 
 function start() {
   log('Photo Archiver is starting...')
   getPath()
   getDirs()
+  checkNext()
 }
 
 start()
