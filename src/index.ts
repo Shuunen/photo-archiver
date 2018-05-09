7d8e7d4ba5cfb19d40249694dfdbebb6b93f7c09
Romain Racamier-Lafon <romain.racamier@gmail.com>
2018-05-09 22:02:18 +0200

    working compression with jpeg-recompress

--- src/index.ts
+++ src/index.ts
@@ -5,13 +5,19 @@ import * as globby from 'globby'
 import inquirer from 'inquirer'
 import * as minimist from 'minimist'
 import { basename, join } from 'path'
+import { exec } from 'child_process'
 
 const argv = minimist(process.argv.slice(2))
 let basepath = process.cwd()
 const dirs = []
+declare const Promise: any;
 
 function log(...thing): void {
   console.log(...thing) // tslint:disable-line:no-console
+}
+
+function warn(...thing): void {
+  console.error(chalk.yellowBright(figures.pointer, ...thing)) // tslint:disable-line:no-console
 }
 
 function error(...thing): void {
@@ -35,33 +41,74 @@ function getDirs() {
   getDirectories(basepath).map((dir) => {
     // dir will be succesivly 2013, 2014,...
     const subDir = join(basepath, dir)
+    // log('subDir', subDir)
     getDirectories(subDir).forEach((sub) => dirs.push(join(subDir, sub)))
   })
-  // log('found dirs', dirs)
+  // if no subdir, just process input dir
+  if (!dirs.length) {
+    dirs.push(basepath)
+  }
+  log('found dir(s)', dirs)
 }
 
-async function checkNext() {
+function compress(photo) {
+  return new Promise((resolve, reject) => {
+    // photo = photo.replace(/\\/g, '/')
+    log('compressing "' + photo + '"')
+    const cmd = 'jpeg-recompress --method smallfry "' + photo + '" "' + photo.replace('.jp', '-c.jp') + '"'
+    log(cmd)
+    exec(cmd, (err, stdout, stderr) => {
+      if (err) {
+        // node couldn't execute the command
+        if (err.toString().indexOf('already processed') !== -1) {
+          resolve('success (already processed)')
+        } else {
+          reject(err)
+        }
+      } else {
+        // the *entire* stdout and stderr (buffered)
+        // console.log(`stdout: ${stdout}`);
+        // console.log(`stderr: ${stderr}`);
+        resolve('success')
+      }
+    });
+  })
+}
+
+function checkPhotos(photos, dir) {
+  log('found', photos.length, 'photos in dir "' + dir.name + '"')
+  // log(photos)
+  compress(photos[0])
+}
+
+function checkNextDir() {
   // extract first
   const dir = dirs.shift() // full path
   const dirName = basename(dir) // directory/folder name
   log('reading dir "' + dirName + '"')
   const dateMatches = dirName.match(/(\d{4})\-(\d{2})/)
+  let year = null
+  let month = null
   if (!dateMatches || !dateMatches.length || dateMatches.length !== 3) {
-    return error('failed at detecting year & month')
+    warn('failed at detecting year & month')
+  } else {
+    year = dateMatches[1]
+    month = dateMatches[2]
+    log('detected year "' + year + '" and month "' + month + '"')
   }
-  const year = dateMatches[1]
-  const month = dateMatches[2]
-  log('detected year "' + year + '" and month "' + month + '"')
-  const photos = await globby(join(dir, '**/*.(jpg|jpeg)'))
-  log('found', photos.length, 'photos')
-  log(photos)
+  let oDir = { name: dirName, year, month }
+  globby(join(dir, '**/*.(jpg|jpeg)'))
+    .then((photos) => checkPhotos(photos, oDir))
+    .catch((err) => {
+      error(err)
+    });
 }
 
 function start() {
   log('Photo Archiver is starting...')
   getPath()
   getDirs()
-  checkNext()
+  checkNextDir()
 }
 
 start()
