1a9eae7df52dcb74e9094ea69bfb12f41e40c244
Romain Racamier-Lafon <romain.racamier@gmail.com>
2018-05-10 21:55:18 +0200

    added config

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import chalk from 'chalk'
 import * as figures from 'figures'
 import { readdirSync, statSync } from 'fs'
 import * as globby from 'globby'
-import inquirer from 'inquirer'
+import * as inquirer from 'inquirer'
 import * as minimist from 'minimist'
 import { basename, join } from 'path'
 import { exec } from 'child_process'
@@ -10,7 +10,20 @@ import { exec } from 'child_process'
 const argv = minimist(process.argv.slice(2))
 let basepath = process.cwd()
 const dirs = []
+let config = {
+  overwrite: false,
+  suffix: '-compressed' // my-photo.jpg => my-photo-compressed.jpg
+}
 declare const Promise: any;
+let photosProcessed = 0
+const questions = [
+  {
+    type: 'confirm',
+    name: 'overwrite',
+    message: 'Overwrite photos when compressing ?',
+    default: config.overwrite
+  },
+]
 
 function log(...thing): void {
   console.log(...thing) // tslint:disable-line:no-console
@@ -54,22 +67,29 @@ function getDirs() {
 function compress(photo) {
   return new Promise((resolve, reject) => {
     // photo = photo.replace(/\\/g, '/')
-    log('compressing "' + photo + '"')
-    const cmd = 'jpeg-recompress --method smallfry "' + photo + '" "' + photo.replace('.jp', '-c.jp') + '"'
-    log(cmd)
-    exec(cmd, (err, stdout, stderr) => {
+    if (photo.indexOf(config.suffix) !== -1) {
+      return resolve('success (already processed)')
+    }
+    // log('compressing "' + photo + '"')
+    const photoIn = photo
+    const photoOut = config.overwrite ? photo : photo.replace('.jp', config.suffix + '.jp')
+    const command = `jpeg-recompress --method smallfry "${photoIn}" "${photoOut}"`
+    // log('executing command :', command)
+    exec(command, (err, stdout, stderr) => {
       if (err) {
         // node couldn't execute the command
-        if (err.toString().indexOf('already processed') !== -1) {
-          resolve('success (already processed)')
-        } else {
-          reject(err)
-        }
+        reject(err)
       } else {
         // the *entire* stdout and stderr (buffered)
         // console.log(`stdout: ${stdout}`);
         // console.log(`stderr: ${stderr}`);
-        resolve('success')
+        if (stderr.toString().indexOf('already processed') !== -1) {
+          resolve('success (already processed)')
+        } else {
+          log('compressed "' + photo + '"')
+          photosProcessed++
+          resolve('success')
+        }
       }
     });
   })
@@ -78,7 +98,8 @@ function compress(photo) {
 function checkPhotos(photos, dir) {
   log('found', photos.length, 'photos in dir "' + dir.name + '"')
   // log(photos)
-  compress(photos[0])
+  photos.forEach(async photo => await compress(photo))
+  log('processed', photosProcessed, 'photos')
 }
 
 function checkNextDir() {
@@ -106,9 +127,13 @@ function checkNextDir() {
 
 function start() {
   log('Photo Archiver is starting...')
-  getPath()
-  getDirs()
-  checkNextDir()
+  inquirer.prompt(questions).then(answers => {
+    config = { ...config, ...answers }
+    log('start with config :', config)
+    getPath()
+    getDirs()
+    checkNextDir()
+  })
 }
 
 start()
