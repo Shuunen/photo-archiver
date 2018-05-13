cefa32bb0237a5584c4a26925393ba64959589ce
Romain Racamier-Lafon <romain.racamier@gmail.com>
2018-05-13 21:07:45 +0200

    now can fix dates

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,14 @@
 import chalk from 'chalk'
+import { exec } from 'child_process'
+import * as ExifTool from 'exiftool-vendored'
 import * as figures from 'figures'
 import { readdirSync, statSync } from 'fs'
 import * as globby from 'globby'
 import * as inquirer from 'inquirer'
 import * as minimist from 'minimist'
 import { basename, join } from 'path'
-import { exec } from 'child_process'
 
+const exiftool = new ExifTool.ExifTool()
 const argv = minimist(process.argv.slice(2))
 const currentPath = process.cwd()
 const dirs = []
@@ -14,22 +16,22 @@ let startTime = null
 let config = {
   basepath: argv.path || currentPath + '/test',
   overwrite: false, // true : will replace original photos / false : will use below suffix and create new files
-  suffix: '-archived' // my-photo.jpg => my-photo-archived.jpg
+  suffix: '-archived', // my-photo.jpg => my-photo-archived.jpg
 }
-declare const Promise: any;
+declare const Promise
 let photosProcessed = 0
 const questions = [
   {
-    type: 'input',
-    name: 'basepath',
+    default: config.basepath,
     message: 'Path to photos ?',
-    default: config.basepath
+    name: 'basepath',
+    type: 'input',
   },
   {
-    type: 'confirm',
-    name: 'overwrite',
+    default: config.overwrite,
     message: 'Overwrite photos when compressing ?',
-    default: config.overwrite
+    name: 'overwrite',
+    type: 'confirm',
   },
 ]
 
@@ -43,8 +45,9 @@ function warn(...things): Promise<string> {
   return Promise.resolve('warn')
 }
 
-function error(...things): void {
+function error(...things): string {
   console.error(chalk.redBright(figures.cross, ...things)) // tslint:disable-line:no-console
+  return things.join(',')
 }
 
 function getDirectories(path) {
@@ -74,6 +77,10 @@ function getDirs() {
   })
 }
 
+function getFinalPhotoName(photo) {
+  return config.overwrite ? photo : photo.replace(/(\.j)/i, config.suffix + '$1')
+}
+
 function compress(photo) {
   return new Promise((resolve, reject) => {
     // photo = photo.replace(/\\/g, '/')
@@ -82,7 +89,7 @@ function compress(photo) {
     }
     // log('compressing "' + photo + '"')
     const photoIn = photo
-    const photoOut = config.overwrite ? photo : photo.replace(/(\.j)/i, config.suffix + '$1')
+    const photoOut = getFinalPhotoName(photo)
     const command = `jpeg-recompress --method smallfry "${photoIn}" "${photoOut}"`
     // log('executing command :', command)
     exec(command, (err, stdout, stderr) => {
@@ -101,25 +108,85 @@ function compress(photo) {
           resolve('success')
         }
       }
-    });
+    })
   })
 }
 
-function compressPhotos(photos) {
-  if (!photos.length) {
-    return Promise.resolve('no more photos in this directory')
-  }
-  // extract first
-  const photo = photos.shift()
-  return compress(photo)
-    .then(() => compressPhotos(photos))
-    .catch(err => error(err))
+function dateToIsoStringWithTimezoneHandling(date) {
+  // from https://stackoverflow.com/a/37661393/1317546
+  return new Date(date.getTime() - (date.getTimezoneOffset() * 60000)).toISOString()
 }
 
-function checkPhotos(photos, dir) {
-  log('found', photos.length, 'photos in dir "' + dir.name + '"')
+function exif(photo: string, dir: DirInfos) {
+  return new Promise((resolve, reject) => {
+    if (!dir.year && !dir.month) {
+      return resolve('cannot fix exif without year and month')
+    }
+    const filepath = getFinalPhotoName(photo)
+    exiftool.read(filepath)
+      .then((tags: ExifTool.Tags) => {
+        const date = new Date(tags.CreateDate + '')
+        const year = date.getFullYear()
+        const month = date.getMonth() + 1
+        let doRewrite = false
+        if (year !== dir.year) {
+          warn('fixing photo year "' + year + '" => "' + dir.year + '"')
+          date.setFullYear(dir.year)
+          doRewrite = true
+        }
+        if (month !== dir.month) {
+          warn('fixing photo month "' + month + '" => "' + dir.month + '"')
+          date.setMonth(dir.month - 1)
+          doRewrite = true
+        }
+        if (doRewrite) {
+          let newDate = dateToIsoStringWithTimezoneHandling(date)
+          if (newDate[newDate.length - 1].toLowerCase() === 'z') {
+            newDate = newDate.substr(0, newDate.length - 1)
+          }
+          log('should rewrite exif with this date : ' + newDate + ', instead of : ' + tags.CreateDate)
+          exiftool
+            .write(filepath, { AllDates: newDate })
+            .then((/* status */) => {
+              // log('exiftool status after writing :', status) // status is undefined :'(
+              resolve('success, updated photo date to : ' + newDate)
+            })
+            .catch(err => {
+              error(err)
+              reject('failed at writing date exif')
+            })
+        } else {
+          resolve('success, date is good')
+        }
+      })
+      .catch(err => {
+        error(err)
+        reject('failed at reading exif')
+      })
+  })
+}
+
+async function checkPhotos(photos: PhotoSet, dir: DirInfos) {
+  const count = photos.length
+  log('found', count, 'photos in dir "' + dir.name + '"')
   // log(photos)
-  return compressPhotos(photos)
+  for (let i = 0; i < count; i++) {
+    const photo = photos[i]
+    let name = basename(photo)
+    if (name.length > 20) {
+      name = name.substr(0, 20) + '...'
+    }
+    const num = i + 1
+    const indent = '  ' + num + ' : '
+    await log('processing photo', num, '(' + name + ')')
+      .then(() => compress(photo))
+      .then(status => log(indent, status))
+      .then(() => exif(photo, dir))
+      .then(status => log(indent, status))
+      .catch(err => error(err))
+  }
+  return Promise.resolve('check photos done in dir "' + dir.name + '"')
+
 }
 
 function checkNextDir() {
@@ -136,13 +203,17 @@ function checkNextDir() {
   if (!dateMatches || !dateMatches.length || dateMatches.length !== 3) {
     warn('failed at detecting year & month')
   } else {
-    year = dateMatches[1]
-    month = dateMatches[2]
+    year = parseInt(dateMatches[1], 10)
+    month = parseInt(dateMatches[2], 10)
     log('detected year "' + year + '" and month "' + month + '"')
   }
-  let oDir = { name: dirName, year, month }
-  return globby(join(dir, '**/*.(jpg|jpeg)'), { nocase: true })
-    .then((photos) => checkPhotos(photos, oDir))
+  const oDir: DirInfos = { name: dirName, year, month }
+  const include = join(dir, '**/*.(jpg|jpeg)')
+  const exclude = '!' + join(dir, '**/*' + config.suffix + '.(jpg|jpeg)')
+  const rules = [include, exclude]
+  // log('search files with rules', rules)
+  return globby(rules, { nocase: true })
+    .then((photos: PhotoSet) => checkPhotos(photos, oDir))
     .then(status => log(status))
     .then(() => checkNextDir())
     .catch(err => error(err))
@@ -161,6 +232,12 @@ function showMetrics() {
   }
 }
 
+function killExifTool() {
+  log('killing exif tool instance...')
+  exiftool.end()
+  return Promise.resolve('success, does not wait for exif-tool killing')
+}
+
 function start() {
   log('Photo Archiver is starting...')
   inquirer.prompt(questions).then(answers => {
@@ -172,7 +249,19 @@ function start() {
       .then(status => log(status))
       .catch((err) => error(err))
       .then(() => showMetrics())
+      .then(() => killExifTool())
+      .catch((err) => error(err))
   })
 }
 
 start()
+
+type PhotoPath = string
+type PhotoSet = PhotoPath[]
+
+interface DirInfos {
+  /** Plop */
+  name: string // folder name
+  year: number
+  month: number
+}
