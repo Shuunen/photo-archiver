6a4989ff0bbc2df8a5bd742afe7a775a03dd7278
Romain Racamier-Lafon <romain.racamier@gmail.com>
2020-11-28 16:06:14 +0100

    bump deps

--- .eslintrc.rules.js
+++ .eslintrc.rules.js
@@ -4,14 +4,14 @@
 module.exports = {
   '@typescript-eslint/no-use-before-define': 'off', // Allows us to hoist variables and functions which I am a fan of, functions not variables that is.
   '@typescript-eslint/member-delimiter-style': ['error', { // Prevents us from using any delimiter for interface properties.
-    'multiline': {
-      'delimiter': 'none',
-      'requireLast': false
+    multiline: {
+      delimiter: 'none',
+      requireLast: false,
     },
-    'singleline': {
-      'delimiter': 'comma',
-      'requireLast': false
-    }
+    singleline: {
+      delimiter: 'comma',
+      requireLast: false,
+    },
   }],
   '@typescript-eslint/indent': 'off', // This is the job of StandardJS, they are competing rules so we turn off the Typescript one.
   'no-unused-vars': 'off', // On the fence about using this one, sometimes we import a package that is never used directly.
